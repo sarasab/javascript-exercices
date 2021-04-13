ee19d89ccb29eac5c12cfb78aaaf361990129c86
sara <sara.sabinm@gmail.com>
2021-04-13 16:58:33 +0200

    exo suite

--- jour-1/firstexo.js
+++ jour-1/firstexo.js
@@ -3,7 +3,7 @@ console.log(`Hello world ! `);
 
 
 //exo 2
-var test = ` nice to meet you`;
+var test = `nice to meet you`;
 console.log(test);
 
 // exo 3
@@ -11,12 +11,56 @@ console.log(test);
 
 var firstName = `SARA`;
 var phrase;
-phrase = firstName +` nice to meet you`;
+phrase =  `nice to meet you `+ firstName ;
 console.log(phrase);
 
 //exo 4
 
+var testLength = `I'm very long !`;
+console.log( testLength.length);
 
+
+
+//exo5
+var melon =`croissant is meh`;
+var melonReplace;
+melonReplace = melon.replace("meh", "good");
+console.log(melonReplace);
+
+// exo6
+var basic = `this is cool`;
+var basicUp = basic.toUpperCase();
+console.log(basicUp);
+
+var basicDown = basic.toLowerCase();
+console.log(basicDown);
+
+// exo7
+
+var word = `kiwi,orange,banane,citron`;
+var letters = word.split(",");
+console.log(letters);
+
+//exo8
+
+var age = 32;
+var template =`im ${age}years old`;
+console.log(template);
+
+//exo 10
+var phrase = `bisou`;
+var phrase1Include = `casse toi pov'con`;
+var phrase2Include =`bisou mon chéri`;
+var falsePhrase= phrase1Include.includes`bisou`;
+var truePhrase=phrase2Include.includes`bisou`;
+
+console.log(falsePhrase,truePhrase);
+
+
+
+
+
+//exo 11
 
 
 
