502d4ccde96e9677742a1b3248c79c549d9fcb90
sara <sara.sabinm@gmail.com>
2021-04-13 15:09:06 +0200

    exo1a4

---
+++ jour-1/firstexo.js
@@ -0,0 +1,22 @@
+// exo 1
+console.log(`Hello world ! `);
+
+
+//exo 2
+var test = ` nice to meet you`;
+console.log(test);
+
+// exo 3
+
+
+var firstName = `SARA`;
+var phrase;
+phrase = firstName +` nice to meet you`;
+console.log(phrase);
+
+//exo 4
+
+
+
+
+
