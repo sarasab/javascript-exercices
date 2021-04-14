7b501d31735bb9ae0cda52f92cbd364ced48a375
sara <sara.sabinm@gmail.com>
2021-04-14 15:06:59 +0200

    day2

---
+++ jour-2/cours2.js
@@ -0,0 +1,54 @@
+//exo 1
+var integer = 102;
+var float = 13.9;
+console.log(integer);
+
+//exo2
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified);
+
+//exo3
+var num = 1.5;
+var rounded = Math.floor(num);
+console.log(rounded);
+
+//exo 4
+var test=12;
+var bis= 5;
+var resultat= test+bis;
+console.log(resultat);
+
+// exo 5
+
+var test=143; 
+var bis= 219;
+console.log(test===bis);
+
+// exo6
+
+var limit= 50;
+var score =64;
+var resultat;
+if (score >= limit){
+    resultat="Ok good !";
+}
+else{
+    resultat="oh nooo....!";
+}
+console.log(resultat);
+ 
+//exo 07
+
+var password = "azerty";
+var passewordok = "5";
+var resultat2;
+if (password > passewordok){
+    resultat2 ="the password is secure";
+}
+console.log(resultat2);
+
+//exo 8
+
+
+
